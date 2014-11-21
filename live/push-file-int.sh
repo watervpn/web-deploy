d9e227441f7ea22bac73186af8c6c66f5e98bf68
alfred <kanalfred@gmail.com>
2014-11-21 04:08:29 -0500

    Update push file to int

--- live/push-file-int.sh
+++ live/push-file-int.sh
@@ -1,13 +1,15 @@
 
 #!/bin/bash
-pushFromPath='/var/www/vhosts/watervpn.com/httpdocs/sites/default/files'
-pushToPath='/var/www/vhosts/watervpn.com/subdomain/int/httpdocs/sites/default/files'
+# File path
+#pushFromPath='/var/www/vhosts/watervpn.com/httpdocs/sites/default/files'
+#pushToPath='/var/www/vhosts/watervpn.com/subdomain/int/httpdocs/sites/default/'
+pushFromPath='/var/www/vhosts/watervpn.com/subdomain/int/httpdocs/sites/default/files'
+pushToPath='/var/www/vhosts/watervpn.com/httpdocs/sites/default/'
+exclude = ''
 
+# Validation
 test ! -d $pushFromPath && echo "pushFrompath doesn't exist" && exit 1
 test ! -d $pushToPath && echo "pushToPath doesn't exist" && exit 1
-#Todo Rsync
-# switch user hostadmin
-#cd /var/www/vhosts/watervpn.com/httpdocs || exit
-## TODO
-#sudo -u hostadmin git pull origin master
-#sudo -u hostadmin git update-server-info
+
+# Rsync
+sudo -u hostadmin rsync -zvr $pushFromPath $pushToPath
